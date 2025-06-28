bff57ddd1c5ee462ca27de007f57fce4cc829503
Fxnarji <fyluft@stud.mediadesign.de>
2025-06-28 21:23:56 +0200

    Adds fish shell configuration
    Configures the fish shell with neofetch, starship,
    and several useful functions and aliases:
    
    - Defines `sudo_last` for repeating the last command with sudo.
    - Adds aliases `sudo!!`, `google`, and `hole`.
    - Creates functions to toggle DNS servers and open the config file.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,35 @@
 if status is-interactive
     neofetch
 end
+
 starship init fish | source
+
+function sudo_last
+    eval sudo $history[1]
+end
+
+alias sudo!! sudo_last
+
+
+function toggle_dns_pi
+    sudo resolvectl dns enp6s0 192.168.178.192
+    notify-send "switching dns to 192.168.178.192"
+    sudo systemd-resolve --flush-caches
+
+
+end
+
+function toggle_dns_google
+    sudo resolvectl dns enp6s0 8.8.4.4
+    notify-send "switching dns to 8.8.4.4"
+    sudo systemd-resolve --flush-caches
+
+end
+
+function fish_config
+    code /home/fxnarji/dotfiles/.config/fish/config.fish
+end
+
+
+alias google toggle_dns_google
+alias hole toggle_dns_pi
